1125ab29bb15054f85aee180d2297b207f303a43
DanaBaraban <33862430+DanaBaraban@users.noreply.github.com>
2018-04-22 22:28:43 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -11,21 +11,21 @@ class CircularInt{
 	
 	CircularInt(int x1, int y1);
 	
-	CircularInt operator+ (const CircularInt &ci);
-	friend CircularInt operator+ ( CircularInt &ci, const int &x);
-	friend CircularInt operator+ (const int &x ,const CircularInt &ci);
-	friend CircularInt operator- (const int &x ,const CircularInt &ci);
+	CircularInt operator+ (const CircularInt &circ);
+	friend CircularInt operator+ ( CircularInt &circ, const int &x);
+	friend CircularInt operator+ (const int &x ,const CircularInt &circ);
+	friend CircularInt operator- (const int &x ,const CircularInt &circ);
 	CircularInt operator* (const int &x);
 	CircularInt& operator*= (const int &x);
-	friend CircularInt operator- (const CircularInt &ci);
+	friend CircularInt operator- (const CircularInt &circ);
 	CircularInt operator- (const int &x);
-	friend CircularInt operator/ (const CircularInt &ci, const int &x);
+	friend CircularInt operator/ (const CircularInt &circ, const int &x);
 	CircularInt& operator+= (const int &x);
 	CircularInt &operator++ ();
 	CircularInt operator++ (int);
 	//ostream &operator<<(ostream &os);
 	
-	//friend ostream &operator<<(ostream &os ,  CircularInt &m);
+	//friend ostream &operator<<(ostream &ost ,  CircularInt &m);
 	
 	void add(int x);
 	void minus(int x);
