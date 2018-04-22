95065cb620922955bb4a3cff8658ad35da5fb047
DanaBaraban <33862430+DanaBaraban@users.noreply.github.com>
2018-04-22 22:14:53 +0300

    Update CircularInt.cpp

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 CircularInt(int x1, int y1);
 	
-CircularInt :: CircularInt operator+ (const CircularInt &circ){}
+CircularInt :: operator+ (const CircularInt &circ){}
 friend CircularInt operator+ ( CircularInt &circ, const int &x){}
 friend CircularInt operator+ (const int &x ,const CircularInt &circ){}
 friend CircularInt operator- (const int &x ,const CircularInt &circ){}
-CircularInt :: CircularInt operator* (const int &x){}
-CircularInt :: CircularInt& operator*= (const int &x){}
+CircularInt :: operator* (const int &x){}
+CircularInt& :: operator*= (const int &x){}
 friend CircularInt operator- (const CircularInt &circ){}
-CircularInt :: CircularInt operator- (const int &x){}
+CircularInt :: operator- (const int &x){}
 friend CircularInt operator/ (const CircularInt &circ, const int &x){}
-CircularInt :: CircularInt& operator+= (const int &x){}
-CircularInt :: CircularInt &operator++ (){}
-CircularInt :: CircularInt operator++ (int){}
+CircularInt& :: operator+= (const int &x){}
+CircularInt& :: operator++ (){}
+CircularInt :: operator++ (int){}
 CircularInt :: ostream &operator<<(ostream &os){}
 friend ostream &operator<<(ostream &ost ,  CircularInt &m){}
 friend std::ostream& operator<<(std::ostream& os, CircularInt const& r){}	
