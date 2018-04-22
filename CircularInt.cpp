536a98a1eacf915f4c7bafd0027d0c4909bb6035
DanaBaraban <33862430+DanaBaraban@users.noreply.github.com>
2018-04-22 22:45:39 +0300

    Update CircularInt.cpp

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 #include "CircularInt.hpp"
 
-CircularInt CircularInt :: (int x1, int y1){}
+CircularInt :: CircularInt (int x1, int y1){}
 	
 CircularInt CircularInt :: operator+ (const CircularInt &circ){}
 CircularInt operator+ ( CircularInt &circ, const int &x){}
@@ -17,7 +17,7 @@ CircularInt operator/ (const CircularInt &circ, const int &x){}
 CircularInt& CircularInt :: operator+= (const int &x){}
 CircularInt& CircularInt :: operator++ (){}
 CircularInt CircularInt :: operator++ (int){}
-CircularInt CircularInt :: ostream &operator<<(ostream &os){}
+ostream &operator<<(ostream &os){}
 ostream &operator<<(ostream &ost ,  CircularInt &m){}
 std::ostream& operator<<(std::ostream& os, CircularInt const& r){}	
 
