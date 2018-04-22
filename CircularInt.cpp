10ab68bb2d7ceaaa9e2f8f628136e0e4a7b2ee3b
DanaBaraban <33862430+DanaBaraban@users.noreply.github.com>
2018-04-22 23:00:29 +0300

    Update CircularInt.cpp

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -5,21 +5,21 @@ using namespace std;
 
 CircularInt :: CircularInt (int x1, int y1){}
 	
-CircularInt CircularInt :: operator+ (const CircularInt &circ){CircularInt ans=circ; return ans;}
-CircularInt operator+ ( CircularInt &circ, const int &x){CircularInt ans=circ; return ans;}
-CircularInt operator+ (const int &x ,const CircularInt &circ){CircularInt ans=circ; return ans;}
-CircularInt operator- (const int &x ,const CircularInt &circ){CircularInt ans=circ; return ans;}
-CircularInt CircularInt :: operator* (const int &x){int ans=x; return ans;}
-CircularInt& CircularInt :: operator*= (const int &x){int ans=x; return ans;}
-CircularInt operator- (const CircularInt &circ){CircularInt ans=circ; return ans;}
-CircularInt CircularInt :: operator- (const int &x){int ans=x; return ans;}
-CircularInt operator/ (const CircularInt &circ, const int &x){CircularInt ans=circ; return ans;}
-CircularInt& CircularInt :: operator+= (const int &x){int ans=x; return ans;}
-CircularInt& CircularInt :: operator++ (){return *this;}
-CircularInt CircularInt :: operator++ (int){int ans; return ans;}
-ostream& CircularInt :: operator<<(ostream &os){ostream ans=os; return ans;}
-ostream& CircularInt :: operator<<(ostream &ost ,  CircularInt &m){ostream ans=ost; return ans;}
-std::ostream& CircularInt :: operator<<(std::ostream& os, CircularInt const& r){CircularInt ans=circ; return ans;}
+CircularInt CircularInt :: operator+ (const CircularInt &circ){CircularInt ans; return ans;}
+CircularInt operator+ ( CircularInt &circ, const int &x){CircularInt ans; return ans;}
+CircularInt operator+ (const int &x ,const CircularInt &circ){CircularInt ans; return ans;}
+CircularInt operator- (const int &x ,const CircularInt &circ){CircularInt ans; return ans;}
+CircularInt CircularInt :: operator* (const int &x){CircularInt ans; return ans;}
+CircularInt& CircularInt :: operator*= (const int &x){CircularInt ans; return ans;}
+CircularInt operator- (const CircularInt &circ){CircularInt ans; return ans;}
+CircularInt CircularInt :: operator- (const int &x){CircularInt ans; return ans;}
+CircularInt operator/ (const CircularInt &circ, const int &x){CircularInt ans; return ans;}
+CircularInt& CircularInt :: operator+= (const int &x){CircularInt ans; return ans;}
+CircularInt& CircularInt :: operator++ (){CircularInt ans; return ans;}
+CircularInt CircularInt :: operator++ (int){CircularInt ans; return ans;}
+ostream& CircularInt :: operator<<(ostream &os){CircularInt ans; return ans;}
+ostream& CircularInt :: operator<<(ostream &ost ,  CircularInt &m){CircularInt ans; return ans;}
+std::ostream& CircularInt :: operator<<(std::ostream& os, CircularInt const& r){CircularInt ans; return ans;}
 
 void CircularInt :: add(int x){}
 void CircularInt :: minus(int x){}
